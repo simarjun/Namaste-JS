b851733ede10638471e97e4a878a2f6d9d413849
simarjun <md268swami1999@gmail.com>
2024-10-25 15:43:11 +0530

    WIP

--- app.js
+++ app.js
@@ -204,3 +204,143 @@
 
 
 //----->settimeout and closures interview questions ---->
+//1.
+
+// function aheh() {
+//     setTimeout(function() {
+//         console.log("hello evryone");
+//     }, 4000)
+// }
+// aheh();
+
+
+// function easa() {
+//     setTimeout (function() {
+
+//         console.log("hello");
+//     }, 2000)
+//     console.log("Hi ");
+// }
+// easa();
+
+
+// function printNum () {
+//     for (let i=1; i<=5; i++) {
+//         setTimeout(function() {
+//             console.log(i)
+//         }, i*1000);
+//     }
+//     console.log("hello");
+// }
+// printNum(); 
+
+
+// function print() {
+//     for (var i=1; i<=5; i++) {
+//         function close(i) {
+//             setTimeout(function() {
+//                 console.log(i);
+//             }, i*1000);
+//         }
+//         close(i);
+//     }
+//     console.log("hi");
+// }
+// print();
+
+
+// 2. inner function forming closure with outer function 
+// function outer () {
+    
+//     function inner () {
+//         console.log(i);
+//     }
+//     let i =10;
+//     return inner;
+// }
+// outer()();
+
+// function outest() {
+//     var c = 20;
+//     function outer(str) {
+//       let a = 10;
+//       function inner() {
+//         console.log(a, c, str);
+//       }
+//       return inner;
+//     }
+//     return outer;
+//   }
+//   outest()("Hello There")(); // 10 20 "Hello There"
+// here inner functions have access to all its outer environment.
+
+// var count = 0;
+// function increment(){
+//   count++;
+//   console.log(count);
+// }
+// console.log(count);
+// increment(); //0, 1
+
+// function counter () {
+//     var count = 1;
+//     return function increment () {
+//         count++;
+//         console.log(count);
+//     }
+// }
+// var counter1 = counter();
+// counter1();
+
+// implementing counter function using constructors.
+// function counter () {
+//     var count = 1;
+//     this.increment = function() {
+//         count++;
+//         console.log(count);
+//     }
+//     this.decrement = function() {
+//         count--;
+//         console.log(count);
+//     }
+// }
+
+// var counter1 = new counter();
+// counter1.increment();
+// counter1.increment();
+// counter1.increment();
+// counter1.decrement();
+
+
+// function a () {
+//     console.log("hello");
+// }
+
+// var b = function() {
+//     console.log("hello2");
+// }
+// a();
+// b();
+
+// var b = function abc() {
+//     console.log("hello");
+// }
+// b();
+// abc(); //it gives error as it was not declared in global scope.
+
+
+//------>Callback Functions and Event Listeners ----->
+// JS is a syncronous and single threaded language deu to callback functions, we can do async things in JS.
+
+setTimeout(function() {
+    console.log("async funcs");
+}, 1000)
+
+function x(y) {
+    console.log("x");
+    y();
+}
+
+x (function y() {
+    console.log("y");
+});
